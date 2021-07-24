f95f3f065042bf491f4137b32eb08ddf74cdd2cb
preet4953 <preet4953@gmail.com>
2021-07-24 21:14:58 +0530

    menu active links working using javaScript in js folder

--- public/javascripts/active.js
+++ public/javascripts/active.js
@@ -1,6 +1,6 @@
 // This program changes the color of active links
 const currentlocation = location.href
-const menuItem=document.querySelectorAll('a');
+const menuItem=document.querySelectorAll('.nav-link');
 const menuLength = menuItem.length;
 for(let i=0; i<menuLength; i++){
     if(menuItem[i].href === currentlocation){
